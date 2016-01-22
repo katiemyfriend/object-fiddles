d66af95989260b593e2a8d7354309b462544a349
Katie Slater <katiemyfriend@hotmail.com>
2016-01-21 20:43:21 -0700

    first commit

--- practice.js
+++ practice.js
@@ -3,7 +3,8 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+var me = {name: "Katie", age: 23};
+alert(me.age);
 
 
 
@@ -15,18 +16,26 @@
 
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+var favoriteThings = {
+  band: "30 Seconds to Mars",
+  food: "Italian",
+  person: "Jared",
+  book: "City of Bones",
+  movie: "Lord of the Rings",
+  holiday: "Christmas"
+}
 
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
+favoriteThings.car = "Jeep";
+favoriteThings.brand = "Disney";
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
-  //Code here
-
+  favoriteThings.food = "Lettuce";
+  favoriteThings.book = "50 Shades of Gray";
 
 
 
@@ -42,7 +51,9 @@ Set the value of that key to 'chapstick'.
 Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
-  //Code here
+var backPack = {};
+var item = "firstPocket";
+// backpack[item] = "chapstick";
 
 //After you do the above, alert your entire backPack object.
 
@@ -64,11 +75,21 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 //Create an 'alsoMe' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
 
-  //Code Here
+var alsoMe = {
+  name: "Katie",
+  age: 23,
+  height: "5ft 6in",
+  gender: "women",
+  married: "yes",
+  eyeColor: "blue",
+  hairColor: "Dead Mouse Brown"
+}
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
-  //Code Here
+for (var propertyName in alsoMe){
+  alert(alsoMe[propertyName]);
+}
 
 
 
@@ -80,11 +101,19 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 //Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
 
-  //Code Here
+var album = {
+  sayHello: "00:02:00",
+  butterfly: "00:02:30",
+  yellowDay: "00:03:05",
+  rainningJoy: "00:01:40",
+  fourHours: "00:04:00"
+}
 
 //Now, loop through your album object alerting every song title individually.
 
-  //Code Here
+for (var propertyName in album){
+  alert(propertyName);
+}
 
 
 
@@ -96,11 +125,21 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 //Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
 
-  //Code Here
+var states = {
+  utah: 100000,
+  idaho: 50000,
+  nevada: 90000,
+  wyoming: 70000,
+  colorado: 95000
+}
 
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
-  //Code Here
+for (var propertyName in states) {
+  if(states[propertyName] > 30000) {
+    alert(propertyName);
+  }
+}
 
 
 
@@ -121,7 +160,11 @@ var user1 = {
 /*Above you're given a user object. Loop through the user object checking to make sure
 that each value is truthy. If it's not truthy, remove it from the object. */
 
-  //Code Here
+// for (var propertyName in user1) {
+//   if (!propertyName) {
+//     delete user1[propertyName];
+//   }
+// }
 
 //Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
@@ -149,7 +192,8 @@ var user2 = {
 //Let's say I, the user, decided to change my name and email address to the following
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
-  //Code Here
+user2.name = "Tyler S. McGinnis";
+user2.email = "tyler.mcginnis@devmounta.in";
 
 //Now call the sayName method that's on the user object which will alert the users email
 
@@ -196,9 +240,9 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 // Create a function called MakeCard which takes in all the data it needs to make a Credit Card object and returns that object so that whenever you invoke MakeCard, you get a brand new credit card.
 
   //Code Here
-  
-  
-  
+
+
+
 //NEXT PROBLEM
 
 
@@ -209,5 +253,3 @@ object. One called 'alertHello' which alerts 'hello' and another method called l
 */
 
   //Code Here
-
-
